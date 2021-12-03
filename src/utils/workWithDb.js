dee32a61b971e1db20e793dbd97a6f37c0bd5727
m-hurski <85618717+m-hurski@users.noreply.github.com>
2021-12-03 21:58:54 +0300

    add started configuration

---
+++ src/utils/workWithDb.js
@@ -0,0 +1,73 @@
+const v4 = require("uuid/dist/esm-browser/v4");
+const {
+  USER_TYPE,
+  BOARD_TYPE,
+  TASK_TYPE,
+  PATH_USER,
+  PATH_BOARD,
+  PATH_TASK
+} = require('../common/constants')
+const user = require('../data/user.json')
+const board = require('../data/board.json')
+const task = require('../data/task.json')
+const {writeDataToFile} = require("./writeDataToFile");
+
+const getDataType = (typeData) => {
+  switch (typeData) {
+    case USER_TYPE:
+      return [user, PATH_USER];
+    case BOARD_TYPE:
+      return [board, PATH_BOARD];
+    case TASK_TYPE:
+      return [task, PATH_TASK];
+    default:
+      return [[], '']
+  }
+}
+
+const findAll = (typeData) => new Promise((resolve) => {
+  resolve(getDataType(typeData)[0])
+})
+
+const findById = (typeData, id) => new Promise((resolve) => {
+  const data = getDataType(typeData)[0].find(db => db.id === id)
+  resolve(data)
+})
+
+const create = (typeData, data) => new Promise((resolve) => {
+  const newPerson = {id: v4(), ...data}
+  const [db, path] = getDataType(typeData)
+
+  db.push(newPerson)
+
+  writeDataToFile(path, db);
+
+  resolve(newPerson)
+})
+
+const update = (typeData, id, data) => new Promise((resolve) => {
+  const [db, path] = getDataType(typeData)
+  const index = db.findIndex((p) => p.id === id)
+
+  db[index] = {id, ...data}
+  writeDataToFile(path, db);
+
+  resolve(db[index])
+})
+
+const remove = (typeData, id) => new Promise((resolve) => {
+  const [db, path] = getDataType(typeData)
+  const dbFiltered = db.filter((p) => p.id !== id)
+
+  writeDataToFile(path, dbFiltered);
+
+  resolve()
+})
+
+module.exports = {
+  findAll,
+  findById,
+  create,
+  update,
+  remove
+}
---
+++ src/utils/writeDataToFile.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+function writeDataToFile(filename, content) {
+  fs.writeFileSync(filename, JSON.stringify(content), 'utf8')
+}
+
+
+module.exports = {
+  writeDataToFile
+}
