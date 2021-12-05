9d82f23b80a3fd6d921d6212469926f4cac74618
m-hurski <85618717+m-hurski@users.noreply.github.com>
2021-12-05 21:33:47 +0300

    refactor with fastify

--- src/resources/columns/column.memory.repository.js
+++
@@ -1,7 +0,0 @@
-const {findAll} = require("../../utils/workWithDb");
-const {BOARD_TYPE} = require("../../data/constants");
-
-const getAll = async () => findAll(BOARD_TYPE);
-
-
-module.exports = { getAll };
--- src/resources/columns/column.service.js
+++
@@ -1,8 +0,0 @@
-const Column = require("./column.model");
-
-const createColumn = (column) =>
-  column.map(({ id, title, order }) => id
-    ? Column.toResponse({ id, title, order })
-    : new Column(title, order));
-
-module.exports = { createColumn };
