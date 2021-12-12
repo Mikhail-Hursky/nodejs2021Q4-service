7878882b10c08b8847fd0ef9a583147a1471e107
Mikhail-Hursky <mikhail.hursky@gmail.com>
2021-12-12 20:33:06 +0300

    update user to ts

---
+++ src/resources/user/user.model.ts
@@ -0,0 +1,24 @@
+import { v4 as uuid } from 'uuid';
+
+export class User {
+  /**
+   * User class represents a custom structure of in-memory database instance
+   * The user represents a person that has a name and login-password pair to login
+   * Every user has its unique identificator created by v4 uuid
+   */
+  id: string;
+
+  name: string;
+
+  login: string;
+
+  password: string | undefined;
+
+  constructor(name: string, login: string, password: string) {
+    this.id = uuid();
+    this.name = name;
+    this.login = login;
+    this.password = password;
+  }
+}
+
